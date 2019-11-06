3c286feb5341e9dd99fb0b933f8d47fd76e72a0c
ApexWilson <wongwilson112@gmail.com>
2019-11-06 02:24:50 +0000

    test

--- js/script.js
+++ js/script.js
@@ -8,4 +8,36 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+$('#shoot').click(function(){
+    let inputVal = $('#input').val
+    $('#userChoice').text(inputVal);
+    $('#computerChoice').text(Math.floor(Math.random().toString(36).substring("Rock","Paper","Scissors")));
+    if(inputVal === "Rock" && computerChoice === "Scissors"){
+            winner = "Player";
+        }
+        if(inputVal === "Rock" && computerChoice === "Paper"){
+            winner = "Computer"
+        }
+        if(inputVal === "Rock" && computerChoice === "Rock"){
+            winner = "No one wins"
+        }
+        if(inputVal === "Scissors" && computerChoice === "Scissors"){
+            winner = "No one wins"
+        }
+        if(inputVal === "Scissors" && computerChoice === "Paper"){
+            winner = "Player";
+        }
+        if(inputVal === "Scissors" && computerChoice === "Rock"){
+            winner = "Computer"
+        }
+        if(inputVal === "Paper" && computerChoice === "Scissors"){
+            winner = "Computer"
+        }
+        if(inputVal === "Paper" && computerChoice === "Paper"){
+            winner = "No one wins"
+        }
+        if(inputVal === "Paper" && computerChoice === "Rock"){
+            winner = "Player";
+        }
+        $('#result').text(winner + " " + "Wins!");
+});
